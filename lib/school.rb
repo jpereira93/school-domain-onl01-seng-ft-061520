c4891e4ee64366e13650f86965c9289e7b8b857c
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 23:22:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  def initialize(school_name)
+    @school = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @student_name
+    @grade
+    @roster << ("student_name", "grade")
+  end
+# school = School.new("Bayside High School")
+end
